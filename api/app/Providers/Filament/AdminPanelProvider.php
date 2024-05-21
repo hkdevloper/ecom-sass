ae6b5d07a3775c7ca7ed4ac6a58abe2c696754f9
hacki <work24to7@gmail.com>
2024-05-21 21:40:31 +0530

    Filament added progress bar

--- api/app/Providers/Filament/AdminPanelProvider.php
+++ api/app/Providers/Filament/AdminPanelProvider.php
@@ -18,6 +18,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Njxqlus\FilamentProgressbar\FilamentProgressbarPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -46,7 +47,10 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                
+
+            ])
+            ->plugins([
+                FilamentProgressbarPlugin::make()->color('#29f'),
             ])
             ->middleware([
                 EncryptCookies::class,
