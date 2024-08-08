24dd08b4de89a7fed832d1517ab5df7291f967a8
hacki <work24to7@gmail.com>
2024-08-08 20:47:19 +0530

    Moved Old code to backup folder

--- api/bootstrap/providers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\Filament\CompanyPanelProvider::class,
-    App\Providers\FortifyServiceProvider::class,
-    App\Providers\JetstreamServiceProvider::class,
-];
